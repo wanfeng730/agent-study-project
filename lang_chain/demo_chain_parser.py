754e9ccd053a8863848f4ed2d3eba20c6e8a21b6
wanfeng730 <2580330397@qq.com>
2026-06-07 18:05:39 +0800

    feat: 链的历史记录

--- lang_chain/demo_chain_parser.py
+++ lang_chain/demo_chain_parser.py
@@ -4,27 +4,34 @@ from typing import Any, Dict
 
 from dotenv import load_dotenv
 
+from fsspec.config import conf
 from langchain.chat_models import init_chat_model
 from langchain.messages import AIMessage, AIMessageChunk, HumanMessage, SystemMessage
 from langchain_classic.prompts import MessagesPlaceholder
 from langchain_classic.schema import StrOutputParser
+from langchain_core.chat_history import InMemoryChatMessageHistory
 from langchain_core.output_parsers import BaseOutputParser
 from langchain_core.prompts import PromptTemplate, FewShotPromptTemplate, ChatPromptTemplate
-from langchain_core.runnables import RunnableLambda
+from langchain_core.runnables import RunnableLambda, RunnableWithMessageHistory
 
 from init_chat_models import *
 
 model = model_qwen_plus()
 print(f'初始化模型：{model}')
 
-
+### 链式调用、格式转换
+"""
+    StrOutputParser：简单字符串解析器，可以作为组件将AIMessage转为str
+    JsonOutputParser：json解析器，可以作为组件将AIMessage转为dict
+"""
 def test_chain():
     prompt_template = PromptTemplate.from_template(
         '我姓{last_name}，刚生了个{gender}，帮我起3个名字，只返回名字并用逗号分隔。'
     )
 
-    # StrOutputParser是langchain内置的简单字符串解析器，可以作为组件将AIMessage转为str
+    # 
     parser = StrOutputParser()
+    # 是langchain内置的
     chain = prompt_template | model | parser | model
 
     # input是一个字典，定义第一个节点调用时的参数，key为参数名，value为参数值
@@ -51,7 +58,16 @@ class ProcessNameOutputParser(BaseOutputParser[Dict[str, Any]]):
             'input_names': text
         }
 
+### 链式调用、自定义格式转换
+"""
+实现自定义格式转换的方法
+    1. RunnableLambda(function)
+        自定义一个function，将AIMessage转换成想要的类型，并用RunnableLambda封装成链的一个节点
+    2. class CustomOutputParser(BaseOutputParser[T])
+        继承BaseOutputParser类，重写parse方法
+        T为泛型，指定返回的类型
 
+"""
 def test_chain_fill_prompt_template():
     prompt_template = PromptTemplate.from_template(
         '我姓{last_name}，刚生了个{gender}，帮我起3个名字，只返回名字并用逗号分隔。'
@@ -74,9 +90,55 @@ def test_chain_fill_prompt_template():
     for message in res:
         if message.content:
             print(message.content, end='', flush=True)
-    
+
+    return
+
+
+
+
+chain_historys = {}
+
+def get_chain_history_in_memory(session_id: str):
+    if session_id not in chain_historys:
+        chain_historys[session_id] = InMemoryChatMessageHistory()
+    return chain_historys[session_id]
+
+
+### 链式调用、带历史记录
+"""
+RunnableWithMessageHistory 参数说明
+    runnable：原链
+    get_session_history：获取会话历史记录的函数
+    input_messages_key：用户输入的占位符名称
+    history_messages_key：填充历史消息的占位符名称
+"""
+def test_chain_with_history():
+    prompt_template = PromptTemplate.from_template(
+        '你需要根据对话历史回答用户问题。对话历史：{history_messages}。用户问题：{user_query}。回答要简洁'
+    )
+    # 链
+    chain = prompt_template | model | StrOutputParser()
+    # 封装成带有历史记录功能的链
+    conversation_chain = RunnableWithMessageHistory(
+        runnable=chain,
+        get_session_history=get_chain_history_in_memory,
+        input_messages_key='user_query',
+        history_messages_key='history_messages'
+    )
+    # 定义会话id
+    config = { 'configurable': { 'session_id': 'user001' }}
+
+    res = conversation_chain.invoke({"user_query": '帮我的女儿起一个好听的名字，姓氏为骆，只回答名字和简要含义即可'}, config=config)
+    print(f'test_chain_with_history 模型返回：{res}')
+    res = conversation_chain.invoke({"user_query": '再想一个其他风格的名字'}, config=config)
+    print(f'test_chain_with_history 模型返回：{res}')
+    res = conversation_chain.invoke({"user_query": '对比一下这两个名字'}, config=config)
+    print(f'test_chain_with_history 模型返回：{res}')
+
+    return
 
 
 print('开始测试')
 # test_chain()
-test_chain_fill_prompt_template()
+# test_chain_fill_prompt_template()
+test_chain_with_history()
