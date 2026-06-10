5dce28e1297cfa21cdbe2dc798f358cb28f97fac
wanfeng730 <2580330397@qq.com>
2026-06-10 15:11:05 +0800

    feat：langchain 向量检索、模型回答

--- lang_chain/demo_rag.py
+++ lang_chain/demo_rag.py
@@ -3,10 +3,13 @@
 from dotenv import load_dotenv
 from langchain_community.embeddings import DashScopeEmbeddings
 from langchain_community.document_loaders import TextLoader
-from langchain_community.embeddings import OllamaEmbeddings
 from langchain_chroma import Chroma
-from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompt_values import PromptValue
+from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from init_chat_models import model_qwen_plus
 
 load_dotenv()
 
@@ -18,7 +21,7 @@ load_dotenv()
 # """
 
 # 阿里云千问embedding向量模型（api_key环境变量：DASHSCOPE_API_KEY）
-embedding_model = DashScopeEmbeddings()
+embedding_model = DashScopeEmbeddings(model="text-embedding-v4")
 
 # Ollama本地部署模型 https://ollama.com/
 # embedding_model = OllamaEmbeddings(model="qwen3-embedding:4b")
@@ -29,6 +32,8 @@ print(f"embedding_model: {embedding_model}")
 # print(f"vector: (向量维度={len(vector)}) {vector}")
 #
 # print((embedding_model.embed_documents(["我喜欢你", "wanfeng"])))
+
+
 
 ### 二、获取文档数据（Document类）
 text_file = 'resources/Teyvathis.md';
@@ -47,7 +52,7 @@ splitter = RecursiveCharacterTextSplitter(
 docs = splitter.split_documents(data)
 print(f'已分割段落：{len(docs)}')
 
-### 二、初始化向量存储
+### 二、初始化向量存储（知识库）
 
 # 初始化存储对象（内存）
 # vector_store = InMemoryVectorStore(embedding=embedding_model)
@@ -68,22 +73,62 @@ vector_store = Chroma(
 doc_ids = [str(index) for index, doc in enumerate(docs)]
 print(f'doc_ids: {doc_ids}')
 vector_store.add_documents(ids=doc_ids, documents=docs)
+# vector_store.add_texts(["内容1", "内容2"])
 
 # 删除文档内容
 delete_ids = ["1", "3"]
 vector_store.delete(delete_ids)
 
-# 检索文档（根据向量相似度匹配）
+
+### 三、构建提示词模版
+system_template = SystemMessagePromptTemplate.from_template("根据如下参考资料，简洁和专业地回答用户问题。参考资料：{context}")
+human_template = HumanMessagePromptTemplate.from_template("用户提问：{query}")
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        # 这里不要直接使用SystemMessage/HumanMessage，这两个是不能格式化的。要么用SystemMessagePromptTemplate，要么用元组直接写
+        system_template,
+        human_template,
+        # ("system", "根据如下参考资料，简洁和专业地回答用户问题。参考资料：{context}"),
+        # ("user", "用户提问：{query}")
+    ]
+)
+
+### 四、用户提问
+user_query = "关于法涅斯的背景是什么"
+
+
+
+
+### 五、根据用户提问检索文档（根据向量相似度匹配）
 # similarity_search() -> list[Document]
 #   query: 查询的文本
 #   k：匹配数量
 #   filter: 过滤Document中元数据的条件，搜索结果只返回符合条件的Document
-search_res = vector_store.similarity_search(
+search_docs = vector_store.similarity_search(
     query="法涅斯",
     k=3,
     filter={ "source": 'resources/Teyvathis.md' }
 )
-print(f"检索结果：{search_res}")
+refer_context = [doc.page_content for doc in search_docs] # 参考文本的格式可以适当优化
+# print(f"检索结果：{refer_context}")
+
+
+
+
+### 六、生成提示词发送模型
+def print_prompt(prompt_value: PromptValue) -> PromptValue:
+    print(f"提示词: {prompt_value}")
+    print()
+    return prompt_value
+
+chat_model = model_qwen_plus()
+chain = prompt_template | print_prompt | chat_model | StrOutputParser()
+
+res = chain.invoke(
+    { "query": user_query, "context": refer_context }
+)
+print(f"回答结果：{res}")
 
 
 
